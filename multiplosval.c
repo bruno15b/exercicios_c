83c5d37201b5ec79e62b5a16b418b46eb699ca02
Bruno Machado <brunomachado3818@gmail.com>
2020-09-02 18:51:17 -0400

    Exercicio1

--- multiplosval.c
+++ multiplosval.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void main(){
+
+    double a,b,c,r;
+
+    printf("Digite os valores a,b,c em sequência:");
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    r=a*b*c;
+
+    printf("O resutade de a*b*c é %.2lf",r);
+
+
+}
