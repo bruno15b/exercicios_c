b201c723051d71f9a26ee7a7bd6648d8012231e9
Bruno Machado <brunomachado3818@gmail.com>
2020-09-02 19:18:55 -0400

    Exercicio3

---
+++ parouimpar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    int num;
+
+    printf("Digite um numero:");
+    scanf("%d", &num);
+
+    if ( num % 2 == 0){
+
+      printf("O número é par.");
+
+    }else{
+
+      printf("O número é impar.");
+
+    }
+
+
+
+
+}
