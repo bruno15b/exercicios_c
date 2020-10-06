6f128470e08614e0a58f4b27b3440893d6d7a129
Bruno Machado <brunomachado3818@gmail.com>
2020-10-06 14:47:19 -0400

    Primeiro exercicio c++

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string palavra;
+    cout << "Informe a palavra:";
+    cin>> palavra;
+    cout << palavra << endl;
+    return 0;
+}
